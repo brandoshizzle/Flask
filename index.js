fcb7ed26d1d6d5f214d57f92986cb6b7e09f8549
brandoshizzle <cathcart.brandon@gmail.com>
2016-12-06 09:36:58 -0700

    -a

---
+++ index.js
@@ -0,0 +1,12 @@
+const electron = require("electron");
+const {app, BrowserWindow} = electron;
+//const buzz = require("buzz");
+
+app.on('ready', function(){
+  var win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  win.loadURL('file://' + __dirname + '/main.html');
+  win.openDevTools();
+})
