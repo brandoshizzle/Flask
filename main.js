95577718d8785d6eb0eec28778afa72423aaa3d9
brandoshizzle <cathcart.brandon@gmail.com>
2016-12-08 21:35:09 -0700

    Beautified, +  responsive square/font-size
    Used vw to size text and btn-key to window width

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 var wavesurfer;
 var keys = $('.btn-key');
 
-function init(){
-  // Create wavesurfer instance
-  wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: 'blue',
-    progressColor: 'purple',
-  });
+function init() {
+    // Create wavesurfer instance
+    wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'blue',
+        progressColor: 'purple',
+    });
 
-  wavesurfer.empty();
+    wavesurfer.empty();
 
 }
 /**
-* Click on button, play/pause audio
-**/
-keys.on('click', function(){
-  wavesurfer.playPause();
+ * Click on button to play/pause audio
+ **/
+keys.on('click', function() {
+    wavesurfer.playPause();
 });
 
 /**
-* Drag and Drop Audio onto keys
-**/
+ * Drag and Drop Audio onto keys
+ **/
 keys.on('dragover', function() {
-  return false;
+    return false;
 });
-keys.on('dragleave',function(){
-  return false;
+keys.on('dragleave', function() {
+    return false;
 });
-keys.on('drop', function(e){
-  e.originalEvent.preventDefault();
-  for (let f of e.originalEvent.dataTransfer.files) {
-    document.getElementById('Q').textContent = f.name;
-    wavesurfer.load(f.path);
-  };
-  return false;
+keys.on('drop', function(e) {
+    e.originalEvent.preventDefault();
+    for (let f of e.originalEvent.dataTransfer.files) {
+        document.getElementById(e.target.id).textContent = f.name;
+        wavesurfer.load(f.path);
+    };
+    return false;
 });
 
 /**
-* Prevent dragging files onto page
-**/
-$(document).on('drop', function(e){
-  e.preventDefault();
-  return false;
+ * Prevent dragging files onto page
+ **/
+$(document).on('drop', function(e) {
+    e.preventDefault();
+    return false;
 });
-$(document).on('dragover', function(e){
-  e.preventDefault();
-  return false;
+$(document).on('dragover', function(e) {
+    e.preventDefault();
+    return false;
 });
