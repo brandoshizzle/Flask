43b08b520a39acddc626746852b6b8761a28239c
brandoshizzle <cathcart.brandon@gmail.com>
2016-12-12 07:50:25 -0700

    Added keyInfo JS object to store/retreive data on loaded songs

--- main.js
+++ main.js
@@ -1,50 +1,77 @@
 var wavesurfer;
 var keys = $('.btn-key');
+var keyInfo = {};
 
 function init() {
-    // Create wavesurfer instance
-    wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: 'blue',
-        progressColor: 'purple',
-    });
-
-    wavesurfer.empty();
-
+	// Create wavesurfer instance
+	wavesurfer = WaveSurfer.create({
+		container: '#waveform',
+		waveColor: 'blue',
+		progressColor: 'purple',
+	});
+	wavesurfer.empty();
 }
 /**
  * Click on button to play/pause audio
  **/
-keys.on('click', function() {
-    wavesurfer.playPause();
+keys.on('click', function(e) {
+	var key = e.target.id;
+	checkKeyInfo(key);
+	wavesurfer.load(keyInfo[key].path);
+	//wavesurfer.playPause();
 });
 
-/**
+keys.keydown(function(e) {
+	var key = e.target.id;
+	wavesurfer.load(keyInfo[key].path);
+	wavesurfer.play();
+});
+
+/********************************
  * Drag and Drop Audio onto keys
- **/
-keys.on('dragover', function() {
-    return false;
+ *******************************/
+keys.on('dragover', function(e) {
+	//$('#' + e.target.id).css("box-shadow", "0px 0px 26px 9px rgba(255,247,99,1)");
+	return false;
 });
 keys.on('dragleave', function() {
-    return false;
+	//$('#' + e.target.id).css("box-shadow", "0px 0px");
+	return false;
 });
 keys.on('drop', function(e) {
-    e.originalEvent.preventDefault();
-    for (let f of e.originalEvent.dataTransfer.files) {
-        document.getElementById(e.target.id).textContent = f.name;
-        wavesurfer.load(f.path);
-    };
-    return false;
+	e.originalEvent.preventDefault(); // Prevent default action
+	for (let f of e.originalEvent.dataTransfer.files) {
+		// grab the id of the target key
+		var key = e.target.id;
+		checkKeyInfo(key);
+		// write file info to array for later
+		keyInfo[key].name = f.name;
+		keyInfo[key].path = f.path;
+		document.getElementById(key).textContent = keyInfo[key].name;
+		wavesurfer.load(keyInfo[key].path);
+	};
+	return false;
 });
 
 /**
  * Prevent dragging files onto page
  **/
 $(document).on('drop', function(e) {
-    e.preventDefault();
-    return false;
+	e.preventDefault();
+	return false;
 });
 $(document).on('dragover', function(e) {
-    e.preventDefault();
-    return false;
+	e.preventDefault();
+	return false;
 });
+
+function checkKeyInfo(key) {
+	try { // test whether the key exists in the info array
+		var test = keyInfo[key].name;
+	} catch (err) { // if it doesn't, create it with empty variables
+		keyInfo[key] = {
+			"name": "",
+			"path": ""
+		}
+	}
+}
