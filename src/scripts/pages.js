1b2d0084642d75bf7bb3d7dc5753f8cc998982a9
brandoshizzle <cathcart.brandon@gmail.com>
2017-01-22 22:31:38 -0700

    Added F1-F8 support, removed bullet nav thing

--- src/scripts/pages.js
+++ src/scripts/pages.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
 	$('ul.tabs').on('click', 'a', function(e) {
-		pageId = e.target.id;
-		pageNum = pageId.substring(pageId.length - 1);
+		var pageId = e.target.id;
+		var pageNum = pageId.substring(pageId.length - 1);
 		$.fn.pagepiling.moveTo(pageNum);
+	});
+	$('body').keydown(function(e){
+		if(e.which > 111 && e.which < 120){
+			var pageNum;
+			switch(e.which){
+				case 112:
+					pageNum = 1;
+					break;
+				case 113:
+					pageNum = 2;
+					break;
+				case 114:
+					pageNum = 3;
+					break;
+				case 115:
+					pageNum = 4;
+					break;
+				case 116:
+					pageNum = 5;
+					break;
+				case 117:
+					pageNum = 6;
+					break;
+				case 118:
+					pageNum = 7;
+					break;
+				case 119:
+					pageNum = 8;
+					break;
+			}
+			$.fn.pagepiling.moveTo(pageNum);
+			$('#page' + pageNum).click();
+		}
 	});
 });
