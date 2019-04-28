097dcd0c7c4e37a9d17518355123681151f902a3
brandoshizzle <cathcart.brandon@gmail.com>
2019-04-28 10:57:08 -0600

    Added splash screen

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /*jshint esversion: 6 */
 const electron = require('electron');
 const { app, BrowserWindow, dialog } = electron;
+const Splashscreen = require('@trodi/electron-splashscreen');
 //var client = require('electron-connect').client;
 
 app.on('ready', function() {
     const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-    const options = {
+    const windowOptions = {
         width: width,
         height: height,
         webPreferences: {
@@ -13,9 +14,33 @@ app.on('ready', function() {
         },
         darkTheme: true
     };
-    const win = new BrowserWindow(options);
-    win.maximize();
+
+    const delay = 2500;
+    let readyToShow = false;
+    const delayTimer = setTimeout(() => {
+        readyToShow = true;
+    }, delay);
+
+    const win = Splashscreen.initSplashScreen({
+        windowOpts: windowOptions,
+        templateUrl: '/build/splash/splash.html',
+        delay: 0, // force show immediately since example will load fast
+        minVisible: delay, // show for 1.5s so example is obvious
+        splashScreenOpts: {
+            height: 500,
+            width: 500,
+            transparent: true
+        }
+    });
+
+    //const win = new BrowserWindow(options);
     win.loadURL('file://' + __dirname + '/src/main.html');
+
+    win.once('show', () => {
+        win.maximize();
+    });
+
+    //
     // Connect to server process
     //client.create(win);
     //win.openDevTools();
